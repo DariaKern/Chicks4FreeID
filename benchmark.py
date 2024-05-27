3055dc29c5d0d3bd4dce4127a28717e5124451e6
Tobias <thetoby@web.de>
2024-05-27 09:36:34 +0200

    Add benchmark files

---
+++ benchmark.py
@@ -0,0 +1,356 @@
+from argparse import ArgumentParser
+from datetime import datetime
+from pathlib import Path
+from typing import Sequence, Union
+
+import torch
+
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.callbacks import (
+    DeviceStatsMonitor,
+    EarlyStopping,
+    LearningRateMonitor,
+)
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
+
+from lightly.data import LightlyDataset
+from lightly.transforms.utils import IMAGENET_NORMALIZE
+from lightly.utils.benchmarking import MetricCallback
+from lightly.utils.dist import print_rank_zero
+
+
+from lightly_benchmarks.imagenet.resnet50 import barlowtwins
+from lightly_benchmarks.imagenet.resnet50 import byol
+from lightly_benchmarks.imagenet.resnet50 import dcl
+from lightly_benchmarks.imagenet.resnet50 import dclw
+from lightly_benchmarks.imagenet.resnet50 import dino
+from lightly_benchmarks.imagenet.resnet50 import finetune_eval
+from lightly_benchmarks.imagenet.resnet50 import knn_eval
+from lightly_benchmarks.imagenet.resnet50 import linear_eval
+from lightly_benchmarks.imagenet.resnet50 import mocov2
+from lightly_benchmarks.imagenet.resnet50 import simclr
+from lightly_benchmarks.imagenet.resnet50 import swav
+from lightly_benchmarks.imagenet.resnet50 import tico
+from lightly_benchmarks.imagenet.resnet50 import vicreg
+
+
+
+
+#### HF TO LIGHTLY DATASET
+
+
+from pathlib import Path
+from typing import Callable, Optional, Union
+from datasets import Dataset
+from datasets import load_dataset
+
+import torchvision
+
+class Chicks4FreeReIDBestTorchVisionDataset(torchvision.datasets.VisionDataset):
+    """
+    Provides the HuggingFace dataset as a Torchvision dataset.
+    The dataset will return a tuple of the input and output based on the supervised_keys.
+    """
+    
+    def __init__(
+        self,
+        root: Union[str, Path] = None,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        download: bool = True,
+    ) -> None:
+        super().__init__(root, transform=transform, target_transform=target_transform)
+
+        self.hf_dataset: Dataset = load_dataset(
+            "dariakern/Chicks4FreeID", 
+            "chicken-re-id-all-visibility", 
+            trust_remote_code=True, 
+            split="train" if train else "test",
+        )
+        self.keys = self.hf_dataset.supervised_keys
+        
+    def __len__(self):
+        return len(self.hf_dataset)
+
+    def __getitem__(self, idx):
+        # Retrieve data at the specified index and return based on supervised_keys
+        data = self.hf_dataset[idx]
+        img = data[self.keys.input]     # Returns pil image
+        target = data[self.keys.output] # Returns an int
+            
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+    
+    def __str__(self):
+        return self
+        
+
+
+
+#####################
+
+
+
+
+
+METHODS = {
+    "barlowtwins": {
+        "model": barlowtwins.BarlowTwins,
+        "transform": barlowtwins.transform,
+    },
+    "byol": {"model": byol.BYOL, "transform": byol.transform},
+    "dcl": {"model": dcl.DCL, "transform": dcl.transform},
+    "dclw": {"model": dclw.DCLW, "transform": dclw.transform},
+    "dino": {"model": dino.DINO, "transform": dino.transform},
+    "mocov2": {"model": mocov2.MoCoV2, "transform": mocov2.transform},
+    "simclr": {"model": simclr.SimCLR, "transform": simclr.transform},
+    "swav": {"model": swav.SwAV, "transform": swav.transform},
+    "tico": {"model": tico.TiCo, "transform": tico.transform},
+    "vicreg": {"model": vicreg.VICReg, "transform": vicreg.transform},
+}
+
+parser = ArgumentParser("Chicks4FreeID ResNet50 Benchmarks")
+#parser.add_argument("--train-dir", type=Path, default="/datasets/imagenet/train", )
+#parser.add_argument("--val-dir", type=Path, default="/datasets/imagenet/val")
+parser.add_argument("--log-dir", type=Path, default="benchmark_logs")
+parser.add_argument("--batch-size-per-device", type=int, default=1)#default=32) #default=128)
+parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--num-workers", type=int, default=4)
+parser.add_argument("--accelerator", type=str, default="gpu")
+parser.add_argument("--devices", type=int, default=1)
+parser.add_argument("--precision", type=str, default="16-mixed")
+parser.add_argument("--ckpt-path", type=Path, default=None)
+parser.add_argument("--compile-model", action="store_true")
+parser.add_argument("--methods", type=str, nargs="+")
+parser.add_argument("--num-classes", type=int, default=50)
+parser.add_argument("--skip-knn-eval", action="store_true")
+parser.add_argument("--skip-linear-eval", action="store_true")
+parser.add_argument("--skip-finetune-eval", action="store_true")
+
+
+
+def main(
+    train_dir: Path,
+    val_dir: Path,
+    log_dir: Path,
+    batch_size_per_device: int,
+    epochs: int,
+    num_workers: int,
+    accelerator: str,
+    devices: int,
+    precision: str,
+    compile_model: bool,
+    methods: Union[Sequence[str], None],
+    num_classes: int,
+    skip_knn_eval: bool,
+    skip_linear_eval: bool,
+    skip_finetune_eval: bool,
+    ckpt_path: Union[Path, None],
+) -> None:
+    torch.set_float32_matmul_precision("high")
+
+    method_names = methods or METHODS.keys()
+
+    for method in method_names:
+        method_dir = (
+            log_dir / method / datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        ).resolve()
+        model = METHODS[method]["model"](
+            batch_size_per_device=batch_size_per_device, num_classes=num_classes
+        )
+
+        if compile_model and hasattr(torch, "compile"):
+            # Compile model if PyTorch supports it.
+            print_rank_zero("Compiling model...")
+            model = torch.compile(model)
+
+        if epochs <= 0:
+            print_rank_zero("Epochs <= 0, skipping pretraining.")
+            if ckpt_path is not None:
+                model.load_state_dict(torch.load(ckpt_path)["state_dict"])
+        else:
+            pretrain(
+                model=model,
+                method=method,
+                train_dir=train_dir,
+                val_dir=val_dir,
+                log_dir=method_dir,
+                batch_size_per_device=batch_size_per_device,
+                epochs=epochs,
+                num_workers=num_workers,
+                accelerator=accelerator,
+                devices=devices,
+                precision=precision,
+                ckpt_path=ckpt_path,
+            )
+
+        if skip_knn_eval:
+            print_rank_zero("Skipping KNN eval.")
+        else:
+            knn_eval.knn_eval(
+                model=model,
+                num_classes=num_classes,
+                train_dir=train_dir,
+                val_dir=val_dir,
+                log_dir=method_dir,
+                batch_size_per_device=batch_size_per_device,
+                num_workers=num_workers,
+                accelerator=accelerator,
+                devices=devices,
+            )
+
+        if skip_linear_eval:
+            print_rank_zero("Skipping linear eval.")
+        else:
+            linear_eval.linear_eval(
+                model=model,
+                num_classes=num_classes,
+                train_dir=train_dir,
+                val_dir=val_dir,
+                log_dir=method_dir,
+                batch_size_per_device=batch_size_per_device,
+                num_workers=num_workers,
+                accelerator=accelerator,
+                devices=devices,
+                precision=precision,
+            )
+
+        if skip_finetune_eval:
+            print_rank_zero("Skipping fine-tune eval.")
+        else:
+            finetune_eval.finetune_eval(
+                model=model,
+                num_classes=num_classes,
+                train_dir=train_dir,
+                val_dir=val_dir,
+                log_dir=method_dir,
+                batch_size_per_device=batch_size_per_device,
+                num_workers=num_workers,
+                accelerator=accelerator,
+                devices=devices,
+                precision=precision,
+            )
+
+
+def pretrain(
+    model: LightningModule,
+    method: str,
+    train_dir: Path,
+    val_dir: Path,
+    log_dir: Path,
+    batch_size_per_device: int,
+    epochs: int,
+    num_workers: int,
+    accelerator: str,
+    devices: int,
+    precision: str,
+    ckpt_path: Union[Path, None],
+) -> None:
+    print_rank_zero(f"Running pretraining for {method}...")
+
+    # Setup training data.
+    train_transform = METHODS[method]["transform"]
+
+    train_dataset = LightlyDataset(input_dir=(train_dir), transform=train_transform)
+    
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size_per_device,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True,
+        persistent_workers=False,
+    )
+
+    # Setup validation data.
+    val_transform = T.Compose(
+        [
+            T.Resize(256),
+            T.CenterCrop(224),
+            T.ToTensor(),
+            T.Normalize(mean=IMAGENET_NORMALIZE["mean"], std=IMAGENET_NORMALIZE["std"]),
+        ]
+    )
+    val_dataset = LightlyDataset(input_dir=(val_dir), transform=val_transform)
+    
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=batch_size_per_device,
+        shuffle=False,
+        num_workers=num_workers,
+        persistent_workers=False,
+    )
+
+    # Train model.
+    metric_callback = MetricCallback()
+    trainer = Trainer(
+        max_epochs=epochs,
+        accelerator="auto",
+        #devices=devices,
+        callbacks=[
+            LearningRateMonitor(),
+            # Stop if training loss diverges.
+            EarlyStopping(monitor="train_loss", patience=int(1e12), check_finite=True),
+            DeviceStatsMonitor(),
+            metric_callback,
+        ],
+        logger=TensorBoardLogger(save_dir=(log_dir), name="pretrain"),
+        precision=precision,
+        #strategy="ddp_find_unused_parameters_true",
+        #sync_batchnorm=accelerator != "cpu",  # Sync batchnorm is not supported on CPU.
+        num_sanity_val_steps=0,
+    )
+
+    trainer.fit(
+        model=model,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=val_dataloader,
+        ckpt_path=ckpt_path,
+    )
+    for metric in ["val_online_cls_top1", "val_online_cls_top5"]:
+        print_rank_zero(f"max {metric}: {max(metric_callback.val_metrics[metric])}")
+
+
+# Monkeypatch LightlyDataset such that __init__ initializes a Chick4FreeReIDBestTorchVisionDataset
+old_init = LightlyDataset.__init__
+def new_init(self, input_dir, transform):
+    try:
+        LightlyDataset.__init__ = old_init
+
+        torch_ds = Chicks4FreeReIDBestTorchVisionDataset(train=input_dir=="train", download=True)
+
+        #from torchvision.datasets import CIFAR10
+        #torch_ds = CIFAR10(root=".", train=input_dir=="train", download=True)
+        # for i in range(10):
+        #    print(torch_ds[i])
+        
+        ds = LightlyDataset.from_torch_dataset(torch_ds, transform)
+        # print(ds[0])
+
+        self.__dict__.update(ds.__dict__)
+    finally:
+        LightlyDataset.__init__ = new_init
+
+
+
+if __name__ == "__main__":
+
+    import lightly.data as data
+
+    args = parser.parse_args()
+    args.train_dir = "train"
+    args.val_dir = "test"
+    LightlyDataset.__init__ = new_init
+
+    #test benchmark
+
+
+    main(**vars(args))
---
+++ lightly_benchmarks/imagenet/vitb16/linear_eval.py
@@ -0,0 +1,149 @@
+from pathlib import Path
+from typing import Tuple
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import DeviceStatsMonitor, LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.nn import BatchNorm1d, Linear, Module, Sequential
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
+
+from lightly.data import LightlyDataset
+from lightly.transforms.utils import IMAGENET_NORMALIZE
+from lightly.utils.benchmarking import LinearClassifier, MetricCallback
+from lightly.utils.lars import LARS
+from lightly.utils.scheduler import CosineWarmupScheduler
+
+
+class LinearEvalClassifier(LinearClassifier):
+    def __init__(
+        self,
+        model: Module,
+        batch_size_per_device: int,
+        feature_dim: int = 2048,
+        num_classes: int = 1000,
+        topk: Tuple[int, ...] = (1, 5),
+        freeze_model: bool = True,
+    ) -> None:
+        super().__init__(
+            model=model,
+            batch_size_per_device=batch_size_per_device,
+            feature_dim=feature_dim,
+            num_classes=num_classes,
+            topk=topk,
+            freeze_model=freeze_model,
+        )
+        # MAE adds an extra batch norm layer before the classification head.
+        self.classification_head = Sequential(
+            BatchNorm1d(feature_dim, affine=False, eps=1e-6),
+            Linear(feature_dim, num_classes),
+        )
+
+    # Adapt optimizer to match MAE settings.
+    def configure_optimizers(self):
+        parameters = list(self.classification_head.parameters())
+        optimizer = LARS(
+            parameters,
+            lr=0.1 * self.batch_size_per_device * self.trainer.world_size / 256,
+            momentum=0.9,
+            weight_decay=0.0,
+        )
+        scheduler = {
+            "scheduler": CosineWarmupScheduler(
+                optimizer=optimizer,
+                warmup_epochs=(
+                    self.trainer.estimated_stepping_batches
+                    / self.trainer.max_epochs
+                    * 10
+                ),
+                max_epochs=self.trainer.estimated_stepping_batches,
+            ),
+            "interval": "step",
+        }
+        return [optimizer], [scheduler]
+
+
+def linear_eval(
+    model: Module,
+    train_dir: Path,
+    val_dir: Path,
+    log_dir: Path,
+    batch_size_per_device: int,
+    num_workers: int,
+    accelerator: str,
+    devices: int,
+    precision: str,
+    num_classes: int,
+) -> None:
+    """Runs a linear evaluation on the given model.
+
+    Parameters follow MAE settings.
+    """
+    print("Running linear evaluation...")
+
+    # Setup training data.
+    train_transform = T.Compose(
+        [
+            T.RandomResizedCrop(224),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize(mean=IMAGENET_NORMALIZE["mean"], std=IMAGENET_NORMALIZE["std"]),
+        ]
+    )
+    train_dataset = LightlyDataset(input_dir=str(train_dir), transform=train_transform)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size_per_device,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True,
+        persistent_workers=True,
+    )
+
+    # Setup validation data.
+    val_transform = T.Compose(
+        [
+            T.Resize(256),
+            T.CenterCrop(224),
+            T.ToTensor(),
+            T.Normalize(mean=IMAGENET_NORMALIZE["mean"], std=IMAGENET_NORMALIZE["std"]),
+        ]
+    )
+    val_dataset = LightlyDataset(input_dir=str(val_dir), transform=val_transform)
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=batch_size_per_device,
+        shuffle=False,
+        num_workers=num_workers,
+        persistent_workers=True,
+    )
+
+    # Train linear classifier.
+    metric_callback = MetricCallback()
+    trainer = Trainer(
+        max_epochs=90,
+        accelerator=accelerator,
+        devices=devices,
+        callbacks=[
+            LearningRateMonitor(),
+            DeviceStatsMonitor(),
+            metric_callback,
+        ],
+        logger=TensorBoardLogger(save_dir=str(log_dir), name="linear_eval"),
+        precision=precision,
+        strategy="ddp_find_unused_parameters_true",
+    )
+    classifier = LinearEvalClassifier(
+        model=model,
+        batch_size_per_device=batch_size_per_device,
+        feature_dim=768,
+        num_classes=num_classes,
+        freeze_model=True,
+    )
+    trainer.fit(
+        model=classifier,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=val_dataloader,
+    )
+    for metric in ["val_top1", "val_top5"]:
+        print(f"max linear {metric}: {max(metric_callback.val_metrics[metric])}")
